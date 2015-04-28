118970cd786e63f21fec4d65aa40f05c180989e8
Anderson Berg <andersonberg@gmail.com>
2015-04-27 23:02:54 -0300

    mudança no layout; lógica de POST na view

--- geo_church/maps/views.py
+++ geo_church/maps/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render_to_response
 
 def map_page(request):
     dict_template = {}
+    if request.method == 'POST':
+        dict_template = {'address': 'Brasil, Recife, Rua da Aurora 1035'}
+
     return render_to_response("map.html", dict_template)
