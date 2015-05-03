de5032c8d3d4ed7e942ad91293a892d9fe8f43b3
Anderson Berg <andersonberg@gmail.com>
2015-05-03 01:07:57 -0300

    modificações para obter a lista de igrejas do banco

--- geo_church/maps/views.py
+++ geo_church/maps/views.py
@@ -1,14 +1,17 @@
 # -*- coding:utf-8 -*-
 
-from django.shortcuts import render
+from __future__ import absolute_import
+# from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from maps.forms import MapForm
+from .forms import MapForm
+from .models import Church
 
 
 def home(request):
     dict_template = {}
-    igrejas = [{'name': 'Templo Central', 'address': 'Brazil, Recife, Avenida Cruz Cabugá 29'}, {'name': 'Campo Grande', 'address': 'Brazil, Recife, Rua Odorico Mendes 327'}, {'name': 'Encruzilhada', 'address': 'Brazil, Recife, Rua Castro Alves 255'}]
+    # igrejas = [{'name': 'Templo Central', 'address': 'Brazil, Recife, Avenida Cruz Cabugá 29'}, {'name': 'Campo Grande', 'address': 'Brazil, Recife, Rua Odorico Mendes 327'}, {'name': 'Encruzilhada', 'address': 'Brazil, Recife, Rua Castro Alves 255'}]
+    igrejas = Church.objects.all()
     dict_template['igrejas'] = igrejas
     return render_to_response("map.html", dict_template, context_instance=RequestContext(request))
 
@@ -16,11 +19,12 @@ def home(request):
 def map_page(request):
     if request.method == 'POST':
         form = MapForm(request.POST)
+        print(form)
         if form.is_valid():
-            address = form.cleaned_data['address']
+            address = "Brazil " + form.cleaned_data['address']
 
             dict_template = {'address': address}
-            igrejas = [{'name': 'Templo Central', 'address': 'Brazil, Recife, Avenida Cruz Cabugá 29'}, {'name': 'Campo Grande', 'address': 'Brazil, Recife, Rua Odorico Mendes 327'}, {'name': 'Encruzilhada', 'address': 'Brazil, Recife, Rua Castro Alves 255'}]
+            igrejas = Church.objects.all()
             dict_template['igrejas'] = igrejas
 
     else:
